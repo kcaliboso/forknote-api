8972e85942996161c4afb81e935416c7d5158c3a
Kevin Caliboso <calibosokevin@gmail.com>
2025-05-28 13:43:44 +0800

    feat: implemented mongodb connection

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,27 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
 import morgan from "morgan";
+
 import router from "./routes";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT ?? "8000";
+
+mongoose
+  .connect(process.env.MONGODB_URL ?? "")
+  .then(() => {
+    console.log("MongoDB connection established.");
+  })
+  .catch((error: unknown) => {
+    if (error instanceof Error) {
+      console.error("Mongoose connection error:", error.message);
+    } else {
+      console.error("Unknown mongoose connection error:", error);
+    }
+  });
 
 app.use(morgan("dev"));
 

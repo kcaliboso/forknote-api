cade578681863cb981e3af0f5bb5300bacbdc5c8
Kevin Caliboso <calibosokevin@gmail.com>
2025-05-28 11:06:14 +0800

    chore: finalize setup for typescript

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,17 @@
-import { Express } from "express";
-import dotenv from "dotenv"
+import express, { type Application } from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import router from "./routes";
 
 dotenv.config();
+
+const app = express();
+const port = process.env.PORT ?? "8000";
+
+app.use(morgan("dev"));
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});

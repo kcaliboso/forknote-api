8ac082d968c8a47af08c5e6af8187c8a900c1f12
Kevin Caliboso <calibosokevin@gmail.com>
2025-05-31 13:48:36 +0800

    refactor: User and Recipe schemas for mongoose to add versionKey and make password for User hidden on select

--- src/models/UserSchema.ts
+++ src/models/UserSchema.ts
@@ -1,29 +1,25 @@
 import { UserDocument } from "#types/models/User.js";
 import mongoose, { Model } from "mongoose";
 
-const userSchema = new mongoose.Schema<UserDocument>(
-  {
-    name: {
-      type: String,
-      required: [true, "A user must have a name"],
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: [true, "A user must have a email"],
-      unique: [true, "Email is already taken"],
-    },
-    password: {
-      type: String,
-      required: [true, "A user must have a password"],
-    },
-    avatar: {
-      type: String,
-    },
+const userSchema = new mongoose.Schema<UserDocument>({
+  name: {
+    type: String,
+    required: [true, "A user must have a name"],
+    trim: true,
   },
-  {
-    versionKey: false,
+  email: {
+    type: String,
+    required: [true, "A user must have a email"],
+    unique: [true, "Email is already taken"],
   },
-);
+  password: {
+    type: String,
+    required: [true, "A user must have a password"],
+    select: false,
+  },
+  avatar: {
+    type: String,
+  },
+});
 
 export const User: Model<UserDocument> = mongoose.model<UserDocument>("User", userSchema);

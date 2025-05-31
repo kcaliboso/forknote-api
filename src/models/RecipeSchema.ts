5727ce25269d3eb56e9fe0a71daba17978c029eb
Kevin Caliboso <calibosokevin@gmail.com>
2025-05-31 17:21:50 +0800

    refactor: registered some query, document and aggregate middlewares on schemas

--- src/models/RecipeSchema.ts
+++ src/models/RecipeSchema.ts
@@ -1,39 +1,73 @@
 import { RecipeDocument } from "#types/models/Recipe.js";
-import mongoose, { Model } from "mongoose";
+import mongoose, { Model, Query } from "mongoose";
 // import User from "./UserSchema";
 
-const recipeSchema = new mongoose.Schema<RecipeDocument>({
-  name: {
-    type: String,
-    required: [true, "A recipe must have a name"],
-    trim: true,
-  },
-  ingredients: {
-    type: [String],
-    required: [true, "A recipe must have ingredients"],
-    validate: {
-      validator: (arr: string[]) => Array.isArray(arr) && arr.length > 0,
-      message: "A recipe must have ingredients",
+const recipeSchema = new mongoose.Schema<RecipeDocument>(
+  {
+    name: {
+      type: String,
+      required: [true, "A recipe must have a name"],
+      trim: true,
+    },
+    ingredients: {
+      type: [String],
+      required: [true, "A recipe must have ingredients"],
+      validate: {
+        validator: (arr: string[]) => Array.isArray(arr) && arr.length > 0,
+        message: "A recipe must have ingredients",
+      },
+    },
+    ratings: {
+      type: Number,
+      default: 0,
+    },
+    images: {
+      type: [String],
+    },
+    cover: {
+      type: String,
+    },
+    // owner: {
+    //   type: User,
+    //   required: [true, "A recipe must have an owner"],
+    // },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
     },
   },
-  ratings: {
-    type: Number,
-    default: 0,
+  {
+    // outputted as json and object, we will append the virtual properties
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  images: {
-    type: [String],
-  },
-  cover: {
-    type: String,
-  },
-  // owner: {
-  //   type: User,
-  //   required: [true, "A recipe must have an owner"],
-  // },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
+);
+
+// Query Middleware Practice
+// Use mongoose Query type and inside that is the result and raw document types, that's
+// why we put two RecipeDocument because our results and raw document is the same.
+recipeSchema.pre<Query<RecipeDocument, RecipeDocument>>(/^find/, function (next) {
+  // remove recipes with 0 ratings
+  // the "this" here refers to the query, so it was just chained.
+  this.find({
+    ratings: {
+      $ne: 0,
+    },
+  });
+  next();
 });
+
+// this also removes it from findById, we can't use findById because
+// it's a wrapper, we need to use the under the hook query which is, findOne
+// but we can just regex the hook name to /^find/, meaning
+// all the hooks that starts with find
+// recipeSchema.pre("findOne", function (next) {
+// });
+
+// Aggregate Middleware
+// recipeSchema.pre("aggregate", function (next) {
+//   this.pipeline().unshift() // this can manipulate the pipeline code we have
+//   next();
+// });
 
 export const Recipe: Model<RecipeDocument> = mongoose.model<RecipeDocument>("Recipe", recipeSchema);
--- src/models/UserSchema.ts
+++ src/models/UserSchema.ts
@@ -1,25 +1,71 @@
 import { UserDocument } from "#types/models/User.js";
 import mongoose, { Model } from "mongoose";
+import argon2 from "argon2";
 
-const userSchema = new mongoose.Schema<UserDocument>({
-  name: {
-    type: String,
-    required: [true, "A user must have a name"],
-    trim: true,
+const userSchema = new mongoose.Schema<UserDocument>(
+  {
+    firstName: {
+      type: String,
+      required: [true, "A user must have a first name"],
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: [true, "A user must have a last name"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "A user must have a email"],
+      unique: [true, "Email is already taken"],
+    },
+    password: {
+      type: String,
+      required: [true, "A user must have a password"],
+      select: false,
+    },
+    avatar: {
+      type: String,
+    },
   },
-  email: {
-    type: String,
-    required: [true, "A user must have a email"],
-    unique: [true, "Email is already taken"],
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
   },
-  password: {
-    type: String,
-    required: [true, "A user must have a password"],
-    select: false,
-  },
-  avatar: {
-    type: String,
-  },
+);
+
+// Virual properties (showcase practice)
+// these are properties that are computed, and is not part of database
+// so we cant use them in queries
+// GETTER
+userSchema.virtual("fullName").get(function () {
+  // we use this keyword here if we need to manipulate existing columns/properties
+  return `${this.firstName} ${this.lastName}`;
 });
+
+// Document Middleware for MongoDB
+// this runs before .save() and .create() (not .insertMany()) to the database
+userSchema.pre("save", async function (next) {
+  const hashedPassword = await argon2.hash(this.password);
+  this.password = hashedPassword;
+
+  next();
+});
+
+// Middlewares are pre('<hook>', function()), post('<hook>', function(doc, next)),
+// save, init, validate, remove, updateOne, deleteOne are all hooks
+// pre and post are the only middleware hooks we have
+
+// Query Middleware
+// allows function to run before or after a certain query
+// looks like document middleware but find is not a document hook,
+// but a query hook
+// userSchema.pre("find", function (next) {
+//   next();
+// });
 
 export const User: Model<UserDocument> = mongoose.model<UserDocument>("User", userSchema);

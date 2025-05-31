39395f5f0c1a70d3226e901105b2f7f8b6f7c5d1
Kevin Caliboso <calibosokevin@gmail.com>
2025-05-31 14:18:09 +0800

    feat: added pagination to Recipe find()

--- src/api/recipe/api.ts
+++ src/api/recipe/api.ts
@@ -2,13 +2,14 @@ import { Recipe } from "#models/RecipeSchema.js";
 import { IndexQueryType } from "#types/filters/RecipeFilters.js";
 import { fields } from "./fields";
 import { filter } from "./filters";
+import { pagination } from "./pagination";
 import { sort } from "./sort";
 
-export const buildQuery = (query: IndexQueryType) => {
+export const buildQuery = async (query: IndexQueryType) => {
   const selectedFields = fields(query);
   const advancedSort = sort(query);
-
   const queryFilter = filter(query);
+  const paginationObj = await pagination(query);
 
-  return Recipe.find(queryFilter).sort(advancedSort).select(selectedFields);
+  return Recipe.find(queryFilter).sort(advancedSort).select(selectedFields).skip(paginationObj.skip).limit(paginationObj.limit);
 };

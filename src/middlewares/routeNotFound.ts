6c5a17c35a55268cd9508af5888855f935bbd1d1
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-03 14:37:13 +0800

    fix: AppErroClass wrong usage

--- src/middlewares/routeNotFound.ts
+++ src/middlewares/routeNotFound.ts
@@ -1,6 +1,6 @@
-import { AppError } from "#utils/appErrorClass.js";
+import { AppErrorClass } from "#utils/appErrorClass.js";
 import type { NextFunction, Request, Response } from "express";
 
 export const routeNotFound = (req: Request, res: Response, next: NextFunction) => {
-  next(new AppError(`Route ${req.originalUrl} cannot be found`, 404));
+  next(new AppErrorClass(`Route ${req.originalUrl} cannot be found`, 404));
 };

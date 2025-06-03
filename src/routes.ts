92d75a5291071d7ef346ddadbbd2fabda6857bdc
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-03 14:36:26 +0800

    feat: registered authRoutes to routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
+import authRoutes from "#routes/authRoutes.js";
 import recipeRoutes from "#routes/recipeRoutes.js";
 import { Router } from "express";
 
 const router = Router();
 
 router.use("/api/v1/recipes", recipeRoutes);
+
+router.use("/api/v1/auth", authRoutes);
 
 export default router;

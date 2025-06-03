468653c888eccfa3127bf988258c8c786b04fe8a
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-03 14:36:56 +0800

    fix: added details on globalErrorHandler to show validation errors

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -2,11 +2,15 @@ import { AppError } from "#types/AppError.js";
 import type { Request, Response, NextFunction } from "express";
 
 export const globalErrorHandler = (error: AppError, _req: Request, res: Response, _next: NextFunction) => {
+  if (error.name === "ValidationError") {
+    error.message = "Validation Error";
+  }
   error.statusCode = error.statusCode ?? 500;
   error.status = error.status ?? "error";
 
   res.status(error.statusCode).json({
     status: error.status,
     message: error.message,
+    details: error.errors,
   });
 };

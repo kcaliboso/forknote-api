8409efc2bde6ef16c1b736f871eacaf69012bd32
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-04 13:24:50 +0800

    feat: added more properties to AppError type

--- src/types/AppError.ts
+++ src/types/AppError.ts
@@ -2,4 +2,9 @@ export interface AppError extends Error {
   errors: [];
   statusCode?: number;
   status?: string;
+  code?: number;
+  name: string;
+  message: string;
+  stack?: string;
+  cause?: unknown;
 }

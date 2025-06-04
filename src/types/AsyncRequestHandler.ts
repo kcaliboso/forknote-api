2a32cce70898c7f19174567fece1be950c83f484
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-04 10:36:53 +0800

    fix: added more type literals for our types and utils

--- src/types/AsyncRequestHandler.ts
+++ src/types/AsyncRequestHandler.ts
@@ -1,17 +1,10 @@
 import type { Request, Response, NextFunction } from "express";
-import ApiResponse from "./responses/ApiResponse";
-import { UserDocument } from "./models/User";
-import { RecipeDocument } from "./models/Recipe";
 import { ParsedQs } from "qs";
 
 import type { ParamsDictionary } from "express-serve-static-core";
 
-type AllowedResBody = ApiResponse<unknown>;
-type AllowedReqBody = UserDocument | RecipeDocument;
-
-export type AsyncRequestHandler<
-  P = ParamsDictionary,
-  ResBody extends AllowedResBody = AllowedResBody,
-  ReqBody extends AllowedReqBody = AllowedReqBody,
-  ReqQuery = ParsedQs,
-> = (req: Request<P, ResBody, ReqBody, ReqQuery>, res: Response<ResBody>, next: NextFunction) => Promise<unknown>;
+export type AsyncRequestHandler<P = ParamsDictionary, ResBody = unknown, ReqBody = unknown, ReqQuery = ParsedQs> = (
+  req: Request<P, ResBody, ReqBody, ReqQuery>,
+  res: Response<ResBody>,
+  next: NextFunction,
+) => Promise<unknown>;
--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -4,14 +4,8 @@ import type { ParamsDictionary } from "express-serve-static-core";
 import type { AsyncRequestHandler } from "#types/AsyncRequestHandler.js";
 import { ParsedQs } from "qs";
 import ApiResponse from "#types/responses/ApiResponse.js";
-import { UserDocument } from "#types/models/User.js";
-import { RecipeDocument } from "#types/models/Recipe.js";
 
-type ResBodies = ApiResponse<unknown>;
-
-type ReqBodies = UserDocument | RecipeDocument;
-
-export const catchAsync = <P = ParamsDictionary, ResBody extends ResBodies = ResBodies, ReqBody extends ReqBodies = ReqBodies, ReqQuery = ParsedQs>(
+export const catchAsync = <P = ParamsDictionary, ResBody = ApiResponse<unknown>, ReqBody = unknown, ReqQuery = ParsedQs>(
   fn: AsyncRequestHandler<P, ResBody, ReqBody, ReqQuery>,
 ): RequestHandler<P, ResBody, ReqBody, ReqQuery> => {
   return (req, res, next) => {

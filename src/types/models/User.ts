862cdad258573758d5718061cfe3de427a20e233
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-04 10:37:25 +0800

    feat: added role on User interface type and added another UserMethods to match UserSchema

--- src/types/models/User.ts
+++ src/types/models/User.ts
@@ -1,3 +1,4 @@
+import { Role } from "#types/enums/Role.js";
 import { Document } from "mongoose";
 
 interface User {
@@ -7,6 +8,11 @@ interface User {
   password: string;
   passwordConfirmation: string | undefined;
   avatar?: string;
+  role: Role;
 }
 
-export interface UserDocument extends User, Document {}
+interface UserMethods {
+  verifyPassword(userHashedPassword: string, inputPassword: string): Promise<boolean>;
+}
+
+export interface UserDocument extends User, Document, UserMethods {}

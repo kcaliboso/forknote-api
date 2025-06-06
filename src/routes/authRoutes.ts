b7798524ce4d3f22f794ca5a49f7620d8b6cbfe9
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-06 11:32:47 +0800

    refactor: authRoutes and removed multer

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,9 @@
 import { signup, login } from "../controllers/authController";
 import { Router } from "express";
-import multer from "multer";
-
-const upload = multer({ dest: "uploads/" });
 
 const authRoutes = Router();
 
-authRoutes.post("/register", [upload.none()], signup);
-authRoutes.post("/login", [upload.none()], login);
+authRoutes.post("/register", signup);
+authRoutes.post("/login", login);
 
 export default authRoutes;

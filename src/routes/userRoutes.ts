d0a8c20edc1a65467a620491fdd6a1564527a78d
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-11 16:59:23 +0800

    fix: changed resizePhotos to resizePhoto

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,7 +2,7 @@ import { upload } from "../config/multerSetup";
 import { deleteCurrentUser, forgotPassword, getUserInfo, resetPassword, updatePassword, updateUser } from "../controllers/userController";
 import { isAuthenticated } from "../middlewares/isAuthenticated";
 import { Router } from "express";
-import { resizePhotos } from "../middlewares/resizePhotos";
+import { resizePhoto } from "../middlewares/resizePhoto";
 
 const userRoutes = Router();
 
@@ -10,7 +10,7 @@ userRoutes.get("/", [isAuthenticated], getUserInfo);
 userRoutes.post("/forgot-password", forgotPassword);
 userRoutes.post("/reset-password/:token", resetPassword);
 userRoutes.post("/update-password", [isAuthenticated], updatePassword);
-userRoutes.patch("/", [isAuthenticated, upload.single("avatar"), resizePhotos], updateUser);
+userRoutes.patch("/", [isAuthenticated, upload.single("avatar"), resizePhoto], updateUser);
 userRoutes.delete("/delete", [isAuthenticated], deleteCurrentUser);
 
 export default userRoutes;

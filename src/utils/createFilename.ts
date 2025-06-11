e9d7fa7cb13e2ff348d85fb1e6e6903f842d7fef
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-11 17:00:02 +0800

    chore: removed unused utility

--- src/utils/createFilename.ts
+++
@@ -1,5 +0,0 @@
-import type { Request } from "express";
-
-export const createFilename = (req: Request) => {
-  return `${req.protocol}://${req.get("host") ?? "localhost"}/uploads/${req.file?.filename ?? "filename.filetype"}`;
-};

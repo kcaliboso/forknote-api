eb8098af6294c43f8b67da4ea3cfcb0258c0b356
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-11 11:20:48 +0800

    feat: added reviews routes on routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import authRoutes from "./routes/authRoutes";
 import recipeRoutes from "./routes/recipeRoutes";
+import reviewRoutes from "./routes/reviewRoutes";
 import userRoutes from "./routes/userRoutes";
 import { Router } from "express";
 
@@ -10,5 +11,7 @@ router.use("/api/v1/recipes", recipeRoutes);
 router.use("/api/v1/auth", authRoutes);
 
 router.use("/api/v1/user", userRoutes);
+
+router.use("/api/v1/reviews", reviewRoutes);
 
 export default router;

3b1299b671f1657b64024c513fafb7d86833c691
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-11 15:52:52 +0800

    feat: using resize middleware for routes with multer

--- src/routes/recipeRoutes.ts
+++ src/routes/recipeRoutes.ts
@@ -5,6 +5,7 @@ import { Role } from "../types/enums/Role";
 import { Router } from "express";
 import { upload } from "../config/multerSetup";
 import reviewRoutes from "./reviewRoutes";
+import { resizePhotos } from "../middlewares/resizePhotos";
 
 const recipeRoutes = Router();
 
@@ -24,12 +25,12 @@ recipeRoutes.route("/ingredients/:ingredient").get(getIngredient);
 recipeRoutes
   .route("/")
   .get(index)
-  .post([upload.single("cover"), isAuthenticated, isAuthorized(Role.Admin, Role.Customer)], store);
+  .post([upload.single("cover"), resizePhotos, isAuthenticated, isAuthorized(Role.Admin, Role.Customer)], store);
 
 recipeRoutes
   .route("/:id")
   .get(show)
-  .patch([upload.single("cover"), isAuthenticated, isAuthorized(Role.Admin, Role.Customer)], update)
+  .patch([upload.single("cover"), resizePhotos, isAuthenticated, isAuthorized(Role.Admin, Role.Customer)], update)
   .delete([isAuthenticated, isAuthorized(Role.Admin, Role.Customer)], destroy);
 
 recipeRoutes.use("/:recipeId/reviews", reviewRoutes);
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,6 +2,7 @@ import { upload } from "../config/multerSetup";
 import { deleteCurrentUser, forgotPassword, getUserInfo, resetPassword, updatePassword, updateUser } from "../controllers/userController";
 import { isAuthenticated } from "../middlewares/isAuthenticated";
 import { Router } from "express";
+import { resizePhotos } from "../middlewares/resizePhotos";
 
 const userRoutes = Router();
 
@@ -9,7 +10,7 @@ userRoutes.get("/", [isAuthenticated], getUserInfo);
 userRoutes.post("/forgot-password", forgotPassword);
 userRoutes.post("/reset-password/:token", resetPassword);
 userRoutes.post("/update-password", [isAuthenticated], updatePassword);
-userRoutes.patch("/", [isAuthenticated, upload.single("avatar")], updateUser);
+userRoutes.patch("/", [isAuthenticated, upload.single("avatar"), resizePhotos], updateUser);
 userRoutes.delete("/delete", [isAuthenticated], deleteCurrentUser);
 
 export default userRoutes;

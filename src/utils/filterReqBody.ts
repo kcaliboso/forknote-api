35610bcd8c06c84bca76b889b90e57a767005263
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-11 15:21:42 +0800

    fix: added trycatch block for filtering req body

--- src/utils/filterReqBody.ts
+++ src/utils/filterReqBody.ts
@@ -1,13 +1,19 @@
+import { AppErrorClass } from "./appErrorClass";
+
 export const filterReqBody = <SourceReqBody extends Record<string, unknown>, AllowedKeys extends keyof SourceReqBody>(
   req: SourceReqBody,
   fields: AllowedKeys[],
 ): Pick<SourceReqBody, AllowedKeys> => {
   const result = {} as Pick<SourceReqBody, AllowedKeys>;
 
-  for (const key of fields) {
-    if (key in req) {
-      result[key] = req[key];
+  try {
+    for (const key of fields) {
+      if (key in req) {
+        result[key] = req[key];
+      }
     }
+  } catch (_error) {
+    throw new AppErrorClass("Something went wrong. Please try again later.", 500);
   }
 
   return result;

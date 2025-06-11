42295bfc2b588435448d847d27dc9755ce29166a
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-11 15:52:12 +0800

    fix: changed multer storage setup to memory instead of auto saving buffer

--- src/config/multerSetup.ts
+++ src/config/multerSetup.ts
@@ -1,6 +1,6 @@
 import multer, { FileFilterCallback } from "multer";
 import type { Request } from "express";
-import path from "path";
+// import path from "path";
 
 const imageFileFilter = (_req: Request, file: Express.Multer.File, callback: FileFilterCallback) => {
   const allowedMimes = ["image/png", "image/jpeg", "image/jpg"];
@@ -12,16 +12,20 @@ const imageFileFilter = (_req: Request, file: Express.Multer.File, callback: Fil
   }
 };
 
-const storage = multer.diskStorage({
-  destination: path.join(__dirname, "../uploads"),
-  filename: (_req: Request, file: Express.Multer.File, callback) => {
-    const ext = path.extname(file.originalname).toLowerCase();
-    const uniqueSuffix = `${Date.now().toString()}-${Math.round(Math.random() * 1e6).toString()}`;
-    const finalName = `file-${uniqueSuffix}${ext}`;
+// do this without resizing
+// const storage = multer.diskStorage({
+//   destination: path.join(__dirname, "../uploads"),
+//   filename: (_req: Request, file: Express.Multer.File, callback) => {
+//     const ext = path.extname(file.originalname).toLowerCase();
+//     const uniqueSuffix = `${Date.now().toString()}-${Math.round(Math.random() * 1e6).toString()}`;
+//     const finalName = `file-${uniqueSuffix}${ext}`;
 
-    callback(null, finalName);
-  },
-});
+//     callback(null, finalName);
+//   },
+// });
+
+// do this if you need to resize
+const storage = multer.memoryStorage();
 
 export const upload = multer({
   storage,
